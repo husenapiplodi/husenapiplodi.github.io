f48d586b54f7a4e2be47fa5fc0d0f751ffa00d4d
husenapiplodi <56085698+husenapiplodi@users.noreply.github.com>
2022-04-28 15:04:22 +0400

    Added new files

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the smallest country in the world?",
+    answer: "Vatican City",
+    options: [
+      "Barbados",
+      "Palau",
+      "Maldives",
+      "Vatican City"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which country touches the Indian Ocean, the Arabian Sea, and the Bay of Bengal?",
+    answer: "India",
+    options: [
+      "India",
+      "UAE",
+      "Malaysia",
+      "Oman"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country consumes the most chocolate per capita?",
+    answer: "Switzerland",
+    options: [
+      "France",
+      "Switzerland",
+      "Netherlands",
+      "Belgium"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What country is responsible for creating the Olympic games?",
+    answer: "Greece",
+    options: [
+      "United States of America",
+      "France",
+      "Greece",
+      "United Kingdom"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which country invented ice cream?",
+    answer: "China",
+    options: [
+      "Turkey",
+      "Belgium",
+      "China",
+      "Italy"
+    ]
+  },
+];
